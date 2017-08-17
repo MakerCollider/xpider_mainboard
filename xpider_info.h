28c7fdb4dfaf89ec5fd03950f18a5e3d5acd0c96
Ye Tian <flymaxty@foxmail.com>
2017-08-17 15:31:47 +0800

    Fix license problem

--- xpider_info.h
+++ xpider_info.h
@@ -1,17 +1,20 @@
-/**
- * Author: Ye Tian <flymaxty@foxmail.com>
- * Copyright (c) 2016 Maker Collider Corporation.
+/*
+ * Xpider mainboard software, running on Intel Curie
+ * Copyright (C) 2015-2017  Roboeve, MakerCollider
  *
- * Permission is hereby granted, free of charge, to any person obtaining
- * a copy of this software and associated documentation files (the
- * "Software"), to deal in the Software without restriction, including
- * without limitation the rights to use, copy, modify, merge, publish,
- * distribute, sublicense, and/or sell copies of the Software, and to
- * permit persons to whom the Software is furnished to do so, subject to
- * the following conditions:
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 #ifndef XPIDER_INFO_H_
